6d5e8a33acd012e6d3840d55d3982dac7dfee631
CN 8 <administrator@CN-8.lan>
2019-03-18 11:39:26 +0000

    first commit

---
+++ public/index.js
@@ -0,0 +1,24 @@
+const submitButton = document.getElementById('submitButton');
+const input = document.getElementById('weatherInput');
+const messageOne = document.getElementById('one');
+const messageTwo = document.getElementById('two');
+
+submitButton.addEventListener('click', (e) => {
+    e.preventDefault();
+   
+    let location = input.value;
+    
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+
+    fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = data.error
+            } else {
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
+            }
+        })
+    })
+})
---
+++ src/app.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const path = require("path");
+const forecast = require("./forecast");
+const geocode = require("./geocode")
+const publicDirectory = path.join(__dirname, "../public")
+
+//init express
+const app = express();
+
+app.use(express.static(publicDirectory))
+
+//gets a string e.g "/" - the "/" can be an "about" or any other string. the function is  a request and response. 
+app.get("/home", (req, res) => {
+    
+    res.send("loaded.")
+    console.log(req.query)
+});
+
+
+
+app.get("/weather", (req, res) => {
+    //requires address in the html bar e.g "?address=Chester"
+    if (!req.query.address){
+        return res.send("Please search for an address")
+    }
+    //requires the address yet again to give the geocode location
+    geocode(req.query.address, (error, response) => {
+        if (error) {
+            return console.log(error)
+        }
+        //requires a latitude and longitude response from geocode and lists the weather report of said location.
+        forecast(response.latitude, response.longitude, (error, forecastData) => {
+            if (error) {
+                return console.log(error)
+            }
+            //sends the data back to browser 
+            res.send({
+                forecast: forecastData,
+                location: response.location,
+                address: req.address
+            })
+        })
+    })
+})
+
+// listens for the port number and runs the function.
+app.listen(3000, () => {
+    console.log("listening on port 3000")
+});
